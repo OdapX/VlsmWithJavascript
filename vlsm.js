34bbef1b2f2fd863933c06975f5be2550149235f
yahya benzha <yahya_benzha@um5.ac.ma>
2021-12-15 17:58:59 +0100

    vlsm algoithm to find subnets with all properties needed

--- vlsm.js
+++ vlsm.js
@@ -1,4 +1,5 @@
-let major_network = "192.168.1.0/24";
+let major_network = "192.168.1.0";
+let CIDR_major = "24";
 const subnets = [
   {
     name: "A",
@@ -14,7 +15,7 @@ const subnets = [
     Twopower: "",
     CIDR: "",
     Mask_decimal: "",
-    dr_reseau: "",
+    adr_reseau: "",
   },
   {
     name: "C",
@@ -22,7 +23,7 @@ const subnets = [
     Twopower: "",
     CIDR: "",
     Mask_decimal: "",
-    dr_reseau: "",
+    adr_reseau: "",
   },
 ];
 
@@ -66,7 +67,45 @@ function Mask_FINDER(subnets) {
   });
 }
 
-function subnet_adress(subnets) {}
+function subnet_adress(subnets) {
+  subnets[0].adr_reseau = major_network;
+  for (let i = 1; i < subnets.length; i++) {
+    subnets[i].adr_reseau = add_adr(
+      subnets[i - 1].Twopower,
+      subnets[i - 1].adr_reseau
+    );
+  }
+}
+
+//this function is used to add the ip adress to a power of two
+// the problem is that the addition should be done to the index
+// resulting from successive division by 2^8 (=1byte)
+//so it's better to have an independent function taking care of that
+
+function add_adr(twopower, adr_reseau) {
+  PowerTwo = 2 ** twopower;
+  j = 1;
+
+  while (PowerTwo % 2 ** 8 == 0) {
+    PowerTwo = PowerTwo / 2 ** 8;
+    j = j + 1;
+  }
+
+  adr_list = adr_reseau.split(".");
+  console.log(adr_list);
+  adr_list[adr_list.length - j] =
+    parseInt(adr_list[adr_list.length - j]) + PowerTwo;
+
+  adress_resaux = "";
+  adr_list.forEach((e) => {
+    adress_resaux = adress_resaux + e + ".";
+  });
+
+  adress_resaux = adress_resaux.slice(0, -1);
+
+  return adress_resaux;
+}
 
 Mask_FINDER(subnets);
+subnet_adress(subnets);
 console.log(subnets);
